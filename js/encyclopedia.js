e2431ca78b230deae6ac8b7d63d0c2c90fb74ed9
Meredith Matthews <mmatthews@mtb.com>
2019-06-15 23:30:40 -0400

    added some matches and refactored to allow one match to create multiple results

--- js/encyclopedia.js
+++ js/encyclopedia.js
@@ -1,46 +1,47 @@
 var encyclopedia = {};
 encyclopedia.fire = {
     active: true,
-    earth: "lava",
-    air: "energy",
-    water: "steam",
-    fire: "heat",
-    mud: "brick"
+    earth: ["lava"],
+    air: ["energy"],
+    water: ["steam"],
+    fire: ["heat"],
+    mud: ["brick"]
 };
 encyclopedia.earth = {
     active: true,
-    fire: "lava",
-    air: "dust",
-    water: "mud",
-    earth: "land",
-    pressure: "rock"
+    fire: ["lava"],
+    air: ["dust"],
+    water: ["mud"],
+    earth: ["land"],
+    pressure: ["rock"],
+    fungus: ["land"]
 };
 encyclopedia.air = {
     active: true,
-    fire: "energy",
-    earth: "dust",
-    water: "rain",
-    air: "pressure",
-    pressure: "wind",
-    energy: "lightning"
+    fire: ["energy"],
+    earth: ["dust"],
+    water: ["rain"],
+    air: ["pressure"],
+    pressure: ["wind"],
+    energy: ["lightning"]
 };
 encyclopedia.water = {
     active: true,
-    fire: "steam",
-    earth: "mud",
-    air: "rain",
-    water: "sea",
-    lava: "obsidian",
-    land: "lake"
+    fire: ["steam"],
+    earth: ["mud"],
+    air: ["rain"],
+    water: ["sea"],
+    lava: ["obsidian"],
+    land: ["lake"]
 };
 encyclopedia.lava = {
     active: false,
-    water: "obsidian"
+    water: ["obsidian"]
 };
 encyclopedia.energy = {
     active: false,
-    wind: "movement",
-    air: "lightning"
+    wind: ["movement"],
+    air: ["lightning"]
 };
 
 encyclopedia.dust = {
@@ -51,66 +52,83 @@ encyclopedia.steam = {
 };
 encyclopedia.mud = {
     active: false,
-    fire: "brick"
+    fire: ["brick"]
 };
 encyclopedia.rain = {
     active: false
 };
 encyclopedia.pressure = {
     active: false,
-    earth: "rock",
-    air: "wind"
+    earth: ["rock"],
+    air: ["wind"]
 };
 encyclopedia.heat = {
     active: false
 };
 encyclopedia.sea = {
-    active: false
+    active: false,
+    life: ["algae"]
 };
 encyclopedia.obsidian = {
     active: false,
-    rock: 'arrowhead'
+    rock: ['arrowhead']
 };
 encyclopedia.rock = {
     active: false,
-    obsidian: "arrowhead"
+    obsidian: ["arrowhead"],
+    life: ["fungus"],
+    fungus: ["land"]
 };
 encyclopedia.arrowhead = {
     active: false
 };
 encyclopedia.land = {
     active: false,
-    water: "lake",
-    river: "swamp"
+    water: ["lake"],
+    river: ["swamp"],
+    algae: ["plants"]
 };
 encyclopedia.brick = {
     active: false
 };
 encyclopedia.lake = {
     active: false,
-    movement: "river"
+    movement: ["river"],
+    life: ["algae"]
 };
 encyclopedia.wind = {
     active: false,
-    wind: "movement",
-    energy: "movement"
+    wind: ["movement"],
+    energy: ["movement"]
 };
 encyclopedia.movement = {
     active: false,
-    lake: "river"
+    lake: ["river"]
 };
 encyclopedia.river = {
     active: false,
-    land: "swamp"
+    land: ["swamp"]
 };
 encyclopedia.swamp = {
     active: false,
-    lightning: "life"
+    lightning: ["life"]
 };
 encyclopedia.lightning = {
     active: false,
-    swamp: "life"
+    swamp: ["life"]
 };
 encyclopedia.life = {
-    active: false
+    active: false,
+    rock: ["fungus"],
+    lake: ["algae"],
+    sea: ["algae"]
+};
+encyclopedia.fungus = {
+    active: false,
+    rock: ["land"],
+    earth: ["land"]
+};
+encyclopedia.algae = {
+    active: false,
+    land: ["plants"]
 };
